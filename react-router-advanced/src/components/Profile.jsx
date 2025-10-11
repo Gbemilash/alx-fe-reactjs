19db931fd07e5501a46375082cb44ad605fc618d
Gbemisola <lasekangbemi@gmail.com>
2025-10-11 18:05:22 +0000

    fix: include nested Routes and ProfileDetails/ProfileSettings imports in Profile.jsx

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
-const Profile = () => (
-  <div>
-    <h1>Profile</h1>
-    <Outlet />
-  </div>
-);
+const Profile = () => {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      {/* Nested Routes */}
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
+      <Outlet />
+    </div>
+  );
+};
 
 export default Profile;
+
