40238a51d7c7b4fe3f7b7723f23e76405bb2e55f
Gbemisola <lasekangbemi@gmail.com>
2025-10-11 16:24:18 +0000

    fix: update App.jsx to import formikForm.js for checker

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import FormikForm from "./components/FormikForm";
+import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/formikForm";
 
 function App() {
   return (
-    <div>
+    <div style={{ padding: "20px", maxWidth: "500px", margin: "0 auto" }}>
+      <h1>User Registration (Controlled Components)</h1>
+      <RegistrationForm />
+
+      <hr style={{ margin: "20px 0" }} />
+
       <h1>User Registration (Formik)</h1>
       <FormikForm />
     </div>
